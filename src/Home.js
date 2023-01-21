03a937be72ce9a2c6fb62ccd770d9f277c10c109
Muhammad Taiyyab <taiyymuh@corp.contoursoftware.net>
2023-01-21 18:23:47 +0500

    Abdul Muqeet did changes in Home.js

--- src/Home.js
+++ src/Home.js
@@ -1,98 +1,78 @@
 import NavbarComponent from "./NavbarComp";
-import { Button, Container} from 'react-bootstrap'
+import { Button, Container } from 'react-bootstrap'
 import './Home.css'
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
-
-
 
 
 function Home() {
 
-    const [longurl,setLongUrl]=  useState('');
-    const [result, setResult]= useState('');
-    const [loader, setLoader]=  useState('false');
-    const [histories, setHistories]=useState([]);
+    const [longurl, setLongUrl] = useState('');
+    const [result, setResult] = useState('');
+    const [loader, setLoader] = useState('false');
+    const [histories, setHistories] = useState([]);
     const [expiryDate, setExpiryDate] = useState('none');
 
-    useEffect (() => {
+    useEffect(() => {
         const localHistories = localStorage.getItem('histories')
-        if(localHistories){
-            setHistories(JSON.parse(localHistories)); 
+        if (localHistories) {
+            setHistories(JSON.parse(localHistories));
         }
 
-    },[setHistories])
+    }, [setHistories])
 
     const fetchData = async () => {
-        try{
-         
-            const res= await axios(`https://api.shrtco.de/v2/shorten?url=${longurl}`)
+        try {
+
+            const res = await axios(`https://api.shrtco.de/v2/shorten?url=${longurl}`)
             setLoader('false')
             console.log(res.data)
-            if (expiryDate === null){
+            if (expiryDate === null) {
                 console.log("enter")
-             }
-             else {
-            setResult(res.data.result.full_short_link)
-            const historiesLog = [...histories, res.data.result, res.data.result.link_date = expiryDate ];
-            setHistories(historiesLog);
-            localStorage.setItem('histories', JSON.stringify(historiesLog));
+            }
+            else {
+                setResult(res.data.result.full_short_link)
+                const historiesLog = [...histories, res.data.result, res.data.result.link_date = expiryDate];
+                setHistories(historiesLog);
+                localStorage.setItem('histories', JSON.stringify(historiesLog));
 
             }
-            
-            
-            
 
-
-        }catch(err) {
+        } catch (err) {
             alert(err);
         }
-    
-    }
-    
-   
-    
 
-    const handleClick = () =>{
+    }
+
+    const handleClick = () => {
         fetchData();
         setLongUrl('');
-        
+
     }
 
     const DateEvent = (e) => {
-        setExpiryDate(e.target.value) 
+        setExpiryDate(e.target.value)
 
     }
 
-    
-    
-    
     return (
         <>
-        <NavbarComponent />
-        <Container id="main-home">
-            <input id="original-urlbox" type="text" placeholder="Enter URL" className="form-control search-input " size="lg" value={longurl} onChange={(e) => setLongUrl(e.target.value)} required/>
-            <Button variant="dark" size="lg" onClick={handleClick}>Click</Button> 
-            {   
-                
-               
-                loader==="true" ? <p>Loading...</p> : <p>{result}</p> 
-           
-            }
-             
 
-          
- 
+            <NavbarComponent />
+            <Container id="main-home">
+                <input id="original-urlbox" type="text" placeholder="Enter URL" className="form-control search-input " size="lg" value={longurl} onChange={(e) => setLongUrl(e.target.value)} required />
+                <Button variant="dark" size="lg" onClick={handleClick}>Click</Button>
+                {
+                    loader === "true" ? <p>Loading...</p> : <p>{result}</p>
+                }
+            </Container>
 
-        </Container>
-        
-        {/* <input type="date" selected={startDate} onChange={(date) => setStartDate(date)} /> */}
-        
-        <input type="date"  onChange={DateEvent} value={expiryDate} />
-        
+            {/* <input type="date" selected={startDate} onChange={(date) => setStartDate(date)} /> */}
+
+            <input type="date" onChange={DateEvent} value={expiryDate} />
+
         </>
         
-
     );
 }
 
